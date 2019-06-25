3852d2b549f04cb4f338946ad00443b21d3a5d73
Sharan-kumar <sharan1638@gmail.com>
2019-06-25 09:37:56 +0530

    linked list insertion

--- linkedlist.c
+++ linkedlist.c
@@ -1,44 +1,38 @@
 #include<stdio.h>
+#include<stdlib.h>
 struct node{
     int data;
     struct node * next;
-}
-struct node * head,tail = NULL;
-head = (struct node * )malloc(sizeof(struct node));
-tail = (struct node * )malloc(sizeof(struct node));
+};
+struct node * head,*tail = NULL;
 
 struct node * insert(int ele){
-    if(head == NULL){
-        head->data = ele;
-        head->next = null;
-        tail = head;
-    }else{
+	    struct node * new;
         new = (struct node*)malloc(sizeof(struct node));
         new->data = ele;
         new->next = NULL;
+    if(head == NULL){
+        head = new;
+        tail = head;
+    }else{
 
         tail->next = new;
         tail = new;
     }
-    return tail;
 }
 
-void printList(){
-    struct node *temp;
-    temp = (struct node *)malloc(sizeof(struct node));
-    temp = head;
+void printList(struct node *temp){
     while(temp!=NULL){
         printf("%d",temp->data);
         temp = temp->next;
     }
 }
-)
+
 void main(){
 
-int arr[] = [1,2,3,4,5];
-struct node * root = NULL;
-for(int i= 0;i<5;i++){
-    root = insert(arr[i]);
+int i,arr[] = {1,2,3,4,5};
+for(i= 0;i<5;i++){
+    insert(arr[i]);
 }
-printList();
+printList(head);
 }
