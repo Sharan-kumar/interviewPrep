0b79c557b00ed460d148bd96d50f784056fcbeb3
Sharan-kumar <sharan1638@gmail.com>
2019-06-25 11:01:30 +0530

    insertion with reverse code

--- linkedlist.c
+++ linkedlist.c
@@ -23,10 +23,45 @@ struct node * insert(int ele){
 
 void printList(struct node *temp){
     while(temp!=NULL){
-        printf("%d",temp->data);
+        printf("%d ",temp->data);
         temp = temp->next;
     }
 }
+
+int lengthList(struct node * temp){
+	int count = 0;
+	while(temp != NULL){
+		count+=1;
+		temp= temp-> next;
+	}
+	return count;
+}
+
+
+void reverseLinkedList(){
+	struct node * temp,* temp1,*rev;
+	int i;
+	temp = temp1 =rev =  (struct node *)malloc(sizeof(struct node));
+	rev = tail;
+	temp = head->next;
+	temp1 = head;
+	while(temp->data != rev->data){
+	while(temp->next->data != rev->data){
+		temp= temp->next;
+		temp1= temp1->next;
+	}
+	temp1->next = temp->next;
+	tail->next = temp;
+	temp->next = NULL;
+	tail = temp;	
+	temp = head->next;
+	temp1 = head; 
+}
+head=temp;
+tail->next  = temp1;
+temp1->next = NULL;
+}
+
 
 void main(){
 
@@ -34,5 +69,10 @@ int i,arr[] = {1,2,3,4,5};
 for(i= 0;i<5;i++){
     insert(arr[i]);
 }
+printList(head);
+printf("\n");
+//printf("%d",lengthList(head));
+reverseLinkedList();
+printf("\n");
 printList(head);
 }
